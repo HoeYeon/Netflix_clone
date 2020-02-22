b561f8853ab7678e24102b82e61a7c319ff8bfea
Hoeyeon <shy9546@naver.com>
2020-02-23 01:45:46 +0900

    feat: CSS

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Router from "Components/Router";
+import GlobalStyles from "Components/GlobalStyles";
+
 function App() {
   return (
     <div>
       <Router></Router>
+      <GlobalStyles></GlobalStyles>
     </div>
   );
 }
